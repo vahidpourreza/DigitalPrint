7a9212bfabba6f8f7c27f93c35fefa17585b8dbc
Vahid Pourreza <vahid.pourreza@outlook.com>
2024-07-15 17:08:44 +0330

    inject Userpfile DI

--- 03.EndPoint/DigitalPrint.EndPoint.API/Program.cs
+++ 03.EndPoint/DigitalPrint.EndPoint.API/Program.cs
@@ -1,7 +1,10 @@
 using DigitalPrint.Core.ApplicationServices.Product.CommandHandlers;
+using DigitalPrint.Core.ApplicationServices.UserProfiles.CommandHandlers;
 using DigitalPrint.Core.Domain.Products.Data;
+using DigitalPrint.Core.Domain.UserProfiles.Data;
 using DigitalPrint.Infrastructures.Data.SqlServer;
 using DigitalPrint.Infrastructures.Data.SqlServer.Product;
+using DigitalPrint.Infrastructures.Data.SqlServer.UserProfiles;
 using Framework.Domain.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -11,21 +14,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>{c.SwaggerDoc("v1", new OpenApiInfo { Title = "Product", Version = "v1" });});
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Product", Version = "v1" }); });
 
-//builder.Services.AddScoped<IProductsRepository, InMemoryProdcutsRepository>();
-builder.Services.AddScoped<IProductsRepository, EfProductsRepository>();
-builder.Services.AddScoped(c => new SqlConnection(builder.Configuration.GetConnectionString("ProductCnn")));
 builder.Services.AddScoped<IUnitOfWork, ProductUnitOfWork>();
+builder.Services.AddScoped(c => new SqlConnection(builder.Configuration.GetConnectionString("ProductCnn")));
 
-builder.Services.AddDbContext<ProductDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductCnn")));
 
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductCnn")));
+
+builder.Services.AddScoped<IProductsRepository, EfProductsRepository>();
 builder.Services.AddScoped<CreateHandler>();
 builder.Services.AddScoped<SetTitleHandler>();
 builder.Services.AddScoped<UpdateDescriptionHandler>();
 builder.Services.AddScoped<UpdatePriceHandler>();
 builder.Services.AddScoped<SendToPublishHandler>();
+
+builder.Services.AddScoped<IUserProfileRepository, EfUserProfileRepository>();
+builder.Services.AddScoped<RegisterUserHandler>();
+builder.Services.AddScoped<UpdateUserNameHandler>();
+builder.Services.AddScoped<UpdateUserEmailHandler>();
+builder.Services.AddScoped<UpdateUserDisplayNameHandler>();
+
 
 var app = builder.Build();
 
