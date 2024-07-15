6b27432f4cc04cb18aff50c49aee2a4a73c43b62
Vahid Pourreza <vahid.pourreza@outlook.com>
2024-07-15 20:00:44 +0330

    fix the event source version

--- 00.Framework/Framework.Domain/Entities/BaseAggregateRoot.cs
+++ 00.Framework/Framework.Domain/Entities/BaseAggregateRoot.cs
@@ -7,12 +7,14 @@ public abstract class BaseAggregateRoot<TId>
 
     private readonly List<IEvent> _events;
     public TId Id { get; protected set; }
+    public long Version { get; private set; } = -1;
     protected BaseAggregateRoot() => _events = new List<IEvent>();
     protected void HandleEvent(IEvent @event)
     {
         SetStateByEvent(@event);
         ValidateInvariants();
         _events.Add(@event);
+        Version++;
     }
     protected abstract void SetStateByEvent(IEvent @event);
     public IEnumerable<IEvent> GetEvents() => _events.AsEnumerable();
